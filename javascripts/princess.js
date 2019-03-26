cd39591749f5d056c3efaf7d7cc01b4761039fc4
samueljdabney <samueljdabney@yahoo.com>
2019-03-25 20:47:19 -0400

    added files

---
+++ javascripts/princess.js
@@ -0,0 +1,231 @@
+
+
+
+
+//==navbar movie reference--//
+
+var data = {
+    0:{
+        name:"Aladdin",
+        pictures:[
+            "https://wallpapercave.com/wp/Wja7xV3.jpg",
+            "https://wallpapercave.com/wp/EArDFry.jpg",
+            "https://wallpapercave.com/wp/tJrmS1R.jpg",
+        ]
+    },
+    1:{
+        name:"Alice in Wonderland",
+        pictures:[
+            "http://whysoblu.com/wp-content/uploads/2011/01/AliceInWonderland60thAnn_Photo_06-1024x773.jpg",
+            "http://warwickartscentre-assets.s3.amazonaws.com/assets/Image/5029.jpg",
+            "http://www.asset1.net/tv/pictures/movie/alice-in-wonderland-1951/Alice-in-Wonderland-DI.jpg",
+        ]
+    },
+    2:{
+        name:"Atlantis",
+        picture:[
+            "https://i.pinimg.com/564x/f0/a2/e6/f0a2e66d2964957d6af37d39519d5fb9.jpg",
+            "https://i.pinimg.com/564x/f0/a2/e6/f0a2e66d2964957d6af37d39519d5fb9.jpg",
+            "https://i.pinimg.com/564x/f0/a2/e6/f0a2e66d2964957d6af37d39519d5fb9.jpg",
+        ]
+    },
+    3:{
+        name:"beauty and beast",
+        pictures:[
+            "https://wallpapercave.com/wp/wp4038698.jpg",
+            "https://wallpapercave.com/wp/wp4038724.jpg",
+            "https://wallpapercave.com/wp/wp4038726.jpg",
+        ]
+    },
+    4:{
+        name:"Brave",
+        pictures:[
+            "https://wallpapercave.com/wp/wp2101272.jpg",
+            "https://wallpapercave.com/wp/wp2101247.jpg",
+            "https://wallpapercave.com/wp/wp1893398.jpg",
+        ]
+    },
+    5:{
+        name:"Cinderella",
+        pictures:[
+            "https://images7.alphacoders.com/335/thumb-1920-335564.jpg",
+            "https://images2.alphacoders.com/203/thumb-1920-203360.jpg",
+            "https://images.alphacoders.com/524/thumb-1920-524828.jpg",
+        ]
+    },
+    6:{
+        name:"Frozen",
+        pictures:[
+            "https://wallpapercave.com/wp/wp2991940.jpg",
+            "https://wallpapercave.com/wp/wp2153230.jpg",
+            "https://wallpapercave.com/wp/wp1809843.jpg",
+        ]
+    },
+    7:{
+        name:"Hercules",
+        pictures:[
+            "http://images4.fanpop.com/image/photos/24400000/Meg-megara-24491654-1280-694.jpg",
+            "https://3.bp.blogspot.com/-GQcq6Vasngw/WPYDzhDiezI/AAAAAAAABuI/sHzxiSvBtKkDzSejH_01TDVI69NQhh1PgCLcB/s1600/EyesMegara.jpg",
+            "https://cdn.wallpapersafari.com/14/29/4wi8FM.png",
+        ]
+    },
+    8:{
+        name:"Moana",
+        pictures:[
+            "https://wallpapercave.com/wp/wp2277899.jpg",
+            "https://wallpapercave.com/wp/wp2277905.jpg",
+            "https://wallpapercave.com/wp/wp2277924.jpg",
+        ]
+    },
+    9:{
+        name:"Mulan",
+        pictures:[
+            "https://wallpapercave.com/wp/vAngNWr.jpg",
+            "https://wallpapercave.com/wp/taHsxts.jpg",
+            "https://wallpapercave.com/wp/xRXlYFn.jpg",
+        ]
+    },
+    10:{
+        name:"Peter Pan",
+        pictures:[
+            "https://wallpapercave.com/wp/4xp8nA0.jpg",
+            "https://wallpapercave.com/wp/ZQk4Ixw.jpg",
+            "https://wallpapercave.com/wp/K3Va5v4.jpg",
+        ]
+    },
+    11:{
+        name:"Pocahontas",
+        pictures:[
+            "https://wallpapercave.com/wp/3o4CxhT.jpg",
+            "https://wallpapercave.com/wp/ms9MRSM.jpg",
+            "https://wallpapercave.com/wp/0E8AT6P.jpg",
+        ]
+    },
+    12:{
+        name:"Sleeping Beauty",
+        pictures:[
+            "https://wallpapercave.com/wp/XjX4O9y.jpg",
+            "https://wallpapercave.com/wp/gv0P0d6.jpg",
+            "https://wallpapercave.com/wp/nqJGKze.jpg",
+        ]
+    },
+    13:{
+        name:"Snow White and the Seven Dwarfs",
+        pictures:[
+            "https://wallpapercave.com/wp/CqmcYyT.jpg",
+            "https://wallpapercave.com/wp/s4YyEUi.jpg",
+            "https://wallpapercave.com/wp/4E7Lz4u.jpg",
+        ]
+    },
+    14:{
+        name:"Tangled",
+        pictures:[
+            "https://wallpapercave.com/wp/wp3221760.png",
+            "https://wallpapercave.com/wp/wp3221769.jpg",
+            "https://wallpapercave.com/wp/wp3221778.jpg",
+        ]
+    },
+    15:{
+        name:"Tarzan",
+        pictures:[
+            "http://www.cartoonswallpapers.net/wallpapers/walt-disney-tarzan-jane-background-image-macbook.jpg",
+            "https://arielhudnall.files.wordpress.com/2015/03/jane-porter-tarzan.jpg",
+            "https://vignette.wikia.nocookie.net/characters/images/b/bb/Tarzan-jane-disneyscreencaps.com-229.jpg/revision/latest?cb=20150518012415",
+            ]
+    },
+    16:{
+        name:"The Hunchback of Notre Dame",
+        pictures:[
+            "http://image.en.yibada.com/data/images/full/71237/esmeralda-from-the-hunchback-of-notre-dame-is-set-to-have-her-live-action-film.jpg",
+            "https://wallpapercave.com/wp/wp2548985.jpg",
+            "https://wallpapercave.com/wp/wp2548934.jpg",
+            ]
+    },
+    17:{
+        name:"The Little Mermaid",
+        pictures:[
+            "https://wallpapercave.com/wp/Qk4yvnv.jpg",
+            "https://wallpapercave.com/wp/wp1844511.png",
+            "https://wallpapercave.com/wp/wp1844506.jpg",
+        ]
+    },
+    18:{
+        name:"The Princess and the Frog",
+        pictures:[
+            "https://wallpapercave.com/wp/wp2271237.png",
+            "https://wallpapercave.com/wp/wp2271279.jpg",
+            "https://wallpapercave.com/wp/wp2271293.jpg",
+        ]
+    },
+    19:{
+        name:"Wreck It Ralph",
+        pictures:[
+            "https://wallpapercave.com/wp/wp1917294.jpg",
+            "https://wallpapercave.com/wp/wp1917246.jpg",
+            "https://wallpapercave.com/wp/wp1917253.jpg",
+        ]
+    },
+
+    
+
+}
+
+
+
+function renderCarousel(e){
+    var Ind = null;
+    var nav_item = document.querySelectorAll('[nav-item-index]');
+    if(e.target){
+       for(let navI in nav_item){
+            let navElm = nav_item[navI];
+            if(navElm && navElm.parentNode && navElm.contains(e.target)){
+                var attr_value = navElm.getAttribute('nav-item-index');
+                attr_value = attr_value && parseInt(attr_value);
+                Ind = attr_value;
+            }
+        } 
+       
+    }else{
+        Ind = e; 
+    }
+    
+    var carouselItems = document.getElementById('carousel-items');
+    while(carouselItems.firstChild){
+        carouselItems.removeChild(carouselItems.firstChild);
+    }
+  
+    
+        data[Ind].pictures.map((s,i)=>{
+            var div2 = document.createElement('div');
+            div2.classList.add('carousel-item');
+            if(i===0){div2.classList.add('active');}
+            var img = document.createElement('img');
+            img.classList.add('d-block');
+            img.classList.add('w-100');
+            img.classList.add('slideImage');
+            img.src=s;
+            div2.appendChild(img);
+            carouselItems.appendChild(div2);
+
+        });
+    
+    
+    
+    console.log('the click is working');
+}
+
+
+
+function init(){
+    var nav_item = document.querySelectorAll('[nav-item-index]');
+     for(let navI in nav_item){
+        let navElm = nav_item[navI];
+        if(navElm && navElm.parentNode){
+            navElm.addEventListener('click',renderCarousel);
+        }
+    }
+    renderCarousel(0);
+}
+
+
+
